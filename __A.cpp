974602ddfe00c5817b4f59eb3433ae1a567c6fca
태윤 김 <ystaeyoon113@yonsei.ac.kr>
2022-03-14 00:47:07 +0900

    basic template

---
+++ __A.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#define fastio std::ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define prntyes cout<<"Yes\n";
+#define prntno cout<<"No\n";
+using namespace std;
+typedef long long ll;
+typedef unsigned long ul;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pii;
+typedef complex<double> inum;
+const double PI = acos(-1);
+const int INF = INT_MAX-1;
+const ll LLINF = 1000000000000000000LL;
+const ll MAX = 200005; // depending on the problem
+const ll MOD = 998244353; // depending on the problem
+
+void solve() {
+    
+
+
+
+
+}
+
+int main() {
+    fastio;
+    int tc = 1;
+    cin >> tc;
+    while (tc--) {
+        solve();
+    }
+
+    return 0;
+}
