9b10de4b5c0f4d58a6af3cfd927346128df0218d
Serendipity-Git <ystaeyoon113@yonsei.ac.kr>
2022-10-02 14:36:04 +0900

    main

---
+++ ___Run.cpp
@@ -0,0 +1,45 @@
+/*###############################################################################################################
+##                                    Author : Kim Tae Yoon (Serendipity__)                                    ##
+###############################################################################################################*/
+
+#include <bits/stdc++.h>
+#define fastio std::ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define prntyes cout<<"Yes\n";
+#define prntno cout<<"No\n";
+using namespace std;
+#ifndef ONLINE_JUDGE
+#include "___debug.h"
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+typedef long long ll;
+typedef unsigned long ul;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pii;
+typedef complex<double> inum;
+const double PI = acos(-1);
+const int INF = 0x3f3f3f3f;
+const ll LLINF = 1000000000000000000LL;
+const ll MAX = 200005; // depending on the problem
+const ll MOD = 998244353; // depending on the problem
+
+void solve() {
+
+
+
+
+
+}
+
+int main() {
+    fastio;
+    int tc = 1;
+    cin >> tc;
+    while (tc--) {
+        solve();
+    }
+    return 0;
+}
